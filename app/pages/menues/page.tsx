51eeb52a4385e0454afb2aa19c01e99b8d112989
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-06-20 12:51:33 +0900

    change login and menu

--- app/pages/menues/page.tsx
+++ app/pages/menues/page.tsx
@@ -8,37 +8,88 @@ import { useEffect, useState } from "react";
 const MenuBeforeLogin = [
   {
     key: 1,
-    title: "User Login",
-    path: "/pages/users/login/user",
-    sub: "일반 회원 로그인",
+    title: "Login",
+    sub: "로그인",
+    children: [
+      {
+        key: 1.1,
+        title: "User Login",
+        path: "/pages/users/login/user",
+        sub: "일반 회원",
+      },
+      {
+        key: 1.2,
+        title: "Lawyer Login",
+        path: "/pages/users/login/lawyer",
+        sub: "변호사 회원",
+      },
+    ],
   },
   {
     key: 2,
-    title: "Lawyer Login",
-    path: "/pages/users/login/lawyer",
-    sub: "변호사 로그인",
+    title: "Boards",
+    sub: "게시판",
+    children: [
+      {
+        key: 2.1,
+        title: "News Boards",
+        path: "/pages/articles/news/all",
+        sub: "뉴스",
+      },
+      {
+        key: 2.2,
+        title: "Lawyers Boards",
+        path: "/pages/articles/lawyers/all",
+        sub: "변호사",
+      },
+      {
+        key: 2.3,
+        title: "Menues",
+        path: "/",
+        sub: "sub",
+      },
+      {
+        key: 2.4,
+        title: "Menues",
+        path: "/",
+        sub: "sub",
+      },
+    ],
   },
-  {
-    key: 3,
-    title: "News",
-    path: "/pages/articles/news/all",
-    sub: "뉴스",
-  },
-  {
-    key: 4,
-    title: "Lawyers",
-    path: "/pages/articles/lawyers/all",
-    sub: "변호사",
-  },
-  { key: 5, title: "Menues", path: "/", sub: "sub" },
-  { key: 6, title: "Menues", path: "/", sub: "sub" },
 ];
 
 const MenuAfterLogin = [
-  { key: 1, title: "Menues", path: "/", sub: "sub" },
-  { key: 2, title: "Menues", path: "/", sub: "sub" },
-  { key: 3, title: "Menues", path: "/", sub: "sub" },
-  { key: 4, title: "Menues", path: "/", sub: "sub" },
+  {
+    key: 2,
+    title: "Boards",
+    sub: "게시판",
+    children: [
+      {
+        key: 2.1,
+        title: "News Boards",
+        path: "/pages/articles/news/all",
+        sub: "뉴스",
+      },
+      {
+        key: 2.2,
+        title: "Lawyers Boards",
+        path: "/pages/articles/lawyers/all",
+        sub: "변호사",
+      },
+      {
+        key: 2.3,
+        title: "Menues",
+        path: "/",
+        sub: "sub",
+      },
+      {
+        key: 2.4,
+        title: "Menues",
+        path: "/",
+        sub: "sub",
+      },
+    ],
+  },
 ];
 
 const MenuPage = (props: any) => {
@@ -62,41 +113,65 @@ const MenuPage = (props: any) => {
   useEffect(() => {
     if (props.menu === true) {
       setAnimate("animate__animated animate__fadeInLeft fast");
-    } else {
-      setAnimate("animate__animated animate__backOutLeft");
     }
   }, [props.menu]);
 
   return (
     <>
       <div
-        className={`${animate} border-r border-[var(--color-Harbor-firth)] fixed top-0 left-0 font-bold text-[var(--color-Harbor-first)] w-[300px] h-screen text-[22px] flex flex-row py-20 px-10`}
+        className={`${animate} border-r border-[var(--color-Harbor-firth)] fixed top-0 left-0 font-bold text-[var(--color-Harbor-first)] w-[350px] h-screen text-[22px] flex flex-row py-20 px-10`}
       >
-        <div className="flex flex-col items-center gap-3">
+        <div className="flex flex-col items-center gap-5">
           {isLoggedIn
             ? MenuAfterLogin.map((item: any) => (
-                <div
-                  key={item.key}
-                  onClick={() => {
-                    router.push(item.path);
-                  }}
-                  className="border-b-2 flex flex-row justify-between items-baseline w-[220px] px-[5px]"
-                >
-                  <h1>{item.title}</h1>
-                  <p className=" font-light text-[14px]">{item.sub}</p>
-                </div>
+                <>
+                  <div
+                    key={item.key}
+                    className="border-b-2 flex flex-row justify-between items-baseline w-[250px] px-[5px]"
+                  >
+                    <h1>{item.title}</h1>
+                    <p className=" font-light text-[14px]">{item.sub}</p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    {item.children.map((child: any) => (
+                      <div
+                        key={child.key}
+                        onClick={() => {
+                          router.push(child.path);
+                        }}
+                        className="flex flex-row justify-between items-baseline w-[240px] px-[5px] text-[14px]"
+                      >
+                        <h1>{child.title}</h1>
+                        <p className=" font-light text-[12px]">{child.sub}</p>
+                      </div>
+                    ))}
+                  </div>
+                </>
               ))
             : MenuBeforeLogin.map((item: any) => (
-                <div
-                  key={item.key}
-                  onClick={() => {
-                    router.push(item.path);
-                  }}
-                  className="border-b-2 flex flex-row justify-between items-baseline w-[220px] px-[5px]"
-                >
-                  <h1>{item.title}</h1>
-                  <p className=" font-light text-[14px]">{item.sub}</p>
-                </div>
+                <>
+                  <div
+                    key={item.key}
+                    className="border-b-2 flex flex-row justify-between items-baseline w-[250px] px-[5px]"
+                  >
+                    <h1>{item.title}</h1>
+                    <p className=" font-light text-[14px]">{item.sub}</p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    {item.children.map((child: any) => (
+                      <div
+                        key={child.key}
+                        onClick={() => {
+                          router.push(child.path);
+                        }}
+                        className="flex flex-row justify-between items-baseline w-[240px] px-[5px] text-[14px]"
+                      >
+                        <h1>{child.title}</h1>
+                        <p className=" font-light text-[12px]">{child.sub}</p>
+                      </div>
+                    ))}
+                  </div>
+                </>
               ))}
         </div>
       </div>
